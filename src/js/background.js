3aab7e359b5672aa58dbdf85c63e141f27a451a7
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-08-30 07:16:49 -0700

    removed unused code from background scripts

--- src/js/background.js
+++ src/js/background.js
@@ -1,13 +1,2 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        // pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
